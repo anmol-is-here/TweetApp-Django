396961e5a93f97b67505d4fa52046cc2a0584148
anmol-is-here <anmolsharmma14055@gmail.com>
2024-09-25 14:56:52 +0530

    check of main

--- tweetapp/tweetapp/urls.py
+++ tweetapp/tweetapp/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth.urls import views as auth_views
-from . import views
+from . import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
